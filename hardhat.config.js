c0be838431266d0005e133310bcddcd59f107503
Luca Brülhart <luca.bruelhart@uzh.ch>
2025-06-05 14:00:35 +0200

    config setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,20 +4,9 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     holesky: {
-      url: "https://eth-holesky.g.alchemy.com/v2/IRAQIiMAtOBoAqvZmwv56",
+      url: process.env.ALCHEMY_API_KEY,
       chainId: 17000,
       accounts: [ process.env.PRIVATE_KEY ]
     }
   }
 };
-
-// localhost: {
-//   url: "http://127.0.0.1:8545",
-//   chainId: 31337
-// },
-
-// holesky: {
-//   url: process.env.HOLESKY_RPC_URL || "https://rpc.holesky.ethpandaops.io",
-//   chainId: 17000,
-//   accounts: [ process.env.PRIVATE_KEY ]
-// }
